01984afb390e7ea1be86263282137deb28cc1e01
Daniel Kistner <kistnerdaniel@pm.me>
2026-01-14 15:16:51 +0100

    removed overflow handling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning={true}>
         <body
-            className={`antialiased overflow-y-auto overflow-x-hidden`}
+            className={`antialiased`}
         >
         <ThemeProvider
             attribute={"class"}
